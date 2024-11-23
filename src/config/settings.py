42b8b6634343bef3087383cdd755e60904eca854
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-11-23 15:06:46 +0400

    add (main app, base templates, base static)

--- src/config/settings.py
+++ src/config/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # local
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
